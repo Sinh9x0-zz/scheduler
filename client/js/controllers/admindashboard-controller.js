7608191d2da70771e20507e64f78eda6e116d65e
Quan Nguyen <minhquan611996@gmail.com>
2015-11-25 13:50:37 -0800

    stuff

--- client/js/controllers/admindashboard-controller.js
+++ client/js/controllers/admindashboard-controller.js
@@ -1,10 +1,10 @@
 app.controller('adminDashController', function(sessionFactory, adminFactory, $location) {
 	var _this = this;
-
+	
 	sessionFactory.getUser(function(currentUser){
 		_this.currentUserData = currentUser;
-		console.log(_this.currentUserData);
-		if(currentUser == ' Require log in') { //if not log in yet
+		_this.currentUser = _this.currentUserData[0].first_name + " " + _this.currentUserData[0].last_name;
+		if(currentUser == ' Require log in' ||  _this.currentUserData.length == 0) { //if not log in yet
 			$location.path('/admin');
 		}
 	})
--- server/controllers/employees.js
+++ server/controllers/employees.js
@@ -40,7 +40,6 @@ module.exports = (function() {
 					password: req.body.password, 
 					first_name: req.body.first_name,
 					last_name: req.body.last_name, 
-					birthday: req.body.birthday, 
 					phone_number: req.body.phone_number,
 					employee_address_id: result.insertId, 
 					created_at: (new Date()).toISOString().substring(0, 19).replace('T', ' '), 
